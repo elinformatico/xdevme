89babc358da50741ba049a248bc2c0d65aff4a8e
elinformatico <donoe1985@gmail.com>
2015-01-29 17:45:33 -0600

    Se comenzo la estructura MVC del proyecto

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    
+    # SET ENVIROMENTS VARIABLES
+    define("DOMAIN", "http://localhost/xdevme.com");
+    define("SERVER_URL", "http://127.0.0.1");
+    define("DEV_PATH_SERVER", "C:/SERVER/wamp/www/xdevme.com");
+    define("PRODUCCTION_PATH_SERVER", "");
+    
+    
---
+++ error404.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title> </title>
+		<style>
+			body{
+				background-color: #0000aa;
+				font-family: "Courier New", Courier, monospace;
+				text-align: center;
+				color: white;
+			}
+		</style>
+	</head>
+	<body>
+		<div style="padding-top:100px;">
+		<h1> Keep calm and error 404! <br /> <br />NOT FOUND</h1>
+		
+		<br />
+		<p>
+			Seguramente has buscando algo que no existe en el server xD <br />
+			Saludos :) <br /><br />
+			Dudas a: <b><a style="color:white;" href="mailto:noe@ranktab.com"> noe@ranktab.com </a></b>
+		</p>
+		</div>
+	</body>
+</html>
